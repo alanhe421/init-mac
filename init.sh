c20301aac44644ffd2d38357c735141101745358
Alan.He <alan@1991421.cn>
2021-08-05 16:00:55 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,7 @@
 # 安装字体图标
 brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
+
+
+# disable  .DS_Store
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
