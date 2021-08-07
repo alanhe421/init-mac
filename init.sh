34172ca06f498ec15fe9e4931b5f1637ce9492f9
Alan.He <alan@1991421.cn>
2021-08-07 14:49:24 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,3 +6,5 @@ brew install --cask font-hack-nerd-font
 
 # disable  .DS_Store
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
+# 恢复设置 
+# defaults delete com.apple.desktopservices DSDontWriteNetworkStores
