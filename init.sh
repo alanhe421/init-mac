f748ef9f33427bcd4e0293d2bf164716787e06fd
Alan.He <alan@1991421.cn>
2021-08-24 09:59:13 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,19 +3,25 @@
 brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
 
-## 安装软件
+# 安装软件
 brew install openconnect
 
 
-# disable  .DS_Store
+## disable  .DS_Store
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
-# 恢复设置 
+## 恢复设置 
 # defaults delete com.apple.desktopservices DSDontWriteNetworkStores
 
+# 映射iCloud配置
 
-# 映射iCloud配置服务
-# wireshark配置
+## wireshark配置
 ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/conf/wireshark ~/.config/wireshark
-# snipaste配置
+## snipaste配置
 ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/conf/snipaste ~/.snipaste
+
+
+# plugin
+## wechat
+curl -o- -L https://raw.githubusercontent.com/lmk123/oh-my-wechat/master/install.sh | bash -s
+
 
